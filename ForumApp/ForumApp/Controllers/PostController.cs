9a9c3706085d67a35c1bb8e92c9cc81f2c8931d8
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-01-27 12:00:01 +0200

    Add project files.

---
+++ ForumApp/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,86 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using ForumApp.Models.Post;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ForumAppDbContext _context;
+
+        public PostController(ForumAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _context.Posts
+                .Select(p => new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Content = p.Content,
+                })
+                .ToListAsync();
+
+            return View(posts);
+        }
+
+        public async Task<IActionResult> Add()
+        => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Add(PostViewModel model)
+        {
+            var post = new Post()
+            {
+                Title = model.Title,
+                Content = model.Content,
+            };
+
+            await _context.Posts.AddAsync(post);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+
+            return View(new PostViewModel()
+            {
+                Title = post.Title,
+                Content = post.Content
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,PostViewModel model)
+        {
+            var post = await _context.Posts.FindAsync(id);
+
+            post.Title = model.Title;
+            post.Content = model.Content;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ForumApp/ForumApp/Models/Post/PostViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ForumApp.Data.DataConstants.PostConst;
+
+namespace ForumApp.Models.Post
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; } = string.Empty;
+    }
+}
